8ea50a29fc2d6ca2e76bbda41c7abb7840fac3c0
marlleyck <marlleyck@hotmail.com>
2022-10-13 19:22:45 -0300

    chore: add file globalColors

---
+++ src/globalColors.ts
@@ -0,0 +1,11 @@
+export const globalColors = {
+    COLORS: {
+        BLUE_PRIMARY: '#5984C0',
+        BLACK: '#1C1C1C',
+        GRAY_20: '#9E9E9E',
+        GRAY_10: '#DFDFDF',
+        GRAY_00: '#F0F0F0',
+        WHITE: '#FFFFFF'
+    }
+};
+
