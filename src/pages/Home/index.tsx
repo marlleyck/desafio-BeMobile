e7200bd056e143f6f0058e528ce027d47c657c04
marlleyck <marlleyck@hotmail.com>
2022-10-13 21:12:35 -0300

    style: add style in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Header } from '../../components/Header';
 
-import {} from './styles';
+import { Header } from '../../components/Header';
+import { Main } from '../../components/Main';
+
+import { useFonts, Roboto_500Medium } from '@expo-google-fonts/roboto';
+
+import {Container} from './styles';
 
 export const Home = () => {
+
+    let [fontsLoaded] = useFonts({
+        Roboto_500Medium
+    })
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <>
+        <Container>
             <Header />
+            <Main />
             <StatusBar style="auto" backgroundColor='white' />
-        </>
+        </Container>
     );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,9 @@
 import styled from 'styled-components/native';
+import { globalTheme } from '../../globalTheme';
+
+export const Container = styled.ScrollView`
+    width: 100%;
+    height: 100%;
+
+    background-color: ${globalTheme.COLORS.GRAY_00};
+`
