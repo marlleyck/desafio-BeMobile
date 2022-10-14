72f7d4a254a1653c424c3fd171f252d8a4bff32c
marlleyck <marlleyck@hotmail.com>
2022-10-13 21:09:32 -0300

    chore: created global theme

---
+++ src/globalTheme.ts
@@ -0,0 +1,14 @@
+export const globalTheme = {
+    COLORS: {
+        BLUE_PRIMARY: '#5984C0',
+        BLACK: '#1C1C1C',
+        GRAY_20: '#9E9E9E',
+        GRAY_10: '#DFDFDF',
+        GRAY_00: '#F0F0F0',
+        WHITE: '#FFFFFF'
+    },
+    FONTS: {
+        Roboto_500Medium: 'Roboto_500Medium'
+    }
+};
+
