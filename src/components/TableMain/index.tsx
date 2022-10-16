24090b56502985d0d2ed86dd9a34310ebf8cf526
marlleyck <marlleyck@hotmail.com>
2022-10-16 00:42:31 -0300

    feat: created component table main

---
+++ src/components/TableMain/index.tsx
@@ -0,0 +1,18 @@
+import { UserType } from "../../types/userType";
+import { UserRow } from "../UserRow";
+
+import {FlatList} from './styles';
+
+type TableMainProps = {
+    users: UserType[] | undefined;
+}
+
+export const TableMain = ({users}: TableMainProps) => {
+    return (
+        <FlatList
+        data={users}
+        keyExtractor={(item: UserType) => item.id}
+        renderItem={({item}: {item: UserType}) => <UserRow userName={item.name} userImage={item.image} /> }
+        />
+    );
+}
---
+++ src/components/TableMain/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const FlatList = styled.FlatList.attrs({
+    contentContainerStyle: {
+        paddingBottom: 120
+    }
+})`
+    flex: 1;
+`
