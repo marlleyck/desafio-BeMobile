389b2fd4073ae43af57caa285a2fa29f5708e9df
marlleyck <marlleyck@hotmail.com>
2022-10-16 00:46:48 -0300

    feat: add new font

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import { Header } from '../../components/Header';
 import { Main } from '../../components/Main';
 
-import { useFonts, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { useFonts, Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 import {Container} from './styles';
 
 export const Home = () => {
 
     let [fontsLoaded] = useFonts({
-        Roboto_500Medium
+        Roboto_500Medium,
+        Roboto_400Regular
     })
     if (!fontsLoaded) {
         return null;
